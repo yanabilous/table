96ad9732b54a9136c5a36980c415954f82123739
yanabilous <yanabilous05@gmail.com>
2023-10-25 00:44:32 +0300

    added task Login

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./pages/Login.jsx";
+import Table from "./pages/Table.jsx";
+
+function App() {
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="table" element={<Table/>}/>
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
